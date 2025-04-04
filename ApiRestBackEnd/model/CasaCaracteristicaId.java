b9f1532d570162f618ae0997b990fe2fdee124f1
Nahir12 <118837735+Nahir12@users.noreply.github.com>
2025-04-04 17:59:39 +0200

    Delete ApiRestBackEnd directory

--- ApiRestBackEnd/model/CasaCaracteristicaId.java
+++
@@ -1,9 +0,0 @@
-package com.dwes.ApiRestBackEnd.model;
-
-import jakarta.persistence.Embeddable;
-
-@Embeddable
-public class CasaCaracteristicaId {
-        private Long casaId;
-        private Long caracteristicaId;
-}
